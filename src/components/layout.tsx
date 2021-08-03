7cd7a7e9a7c2a8cb2b4e44480d30ae34f95a0270
M.D. van Es <es@mdworld.nl>
2021-08-03 16:03:08 +0200

    Add some articles

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,25 @@
 import * as React from "react";
-import { Link, useStaticQuery, graphql } from "gatsby";
-import { CssBaseline, ThemeProvider, Typography } from "@material-ui/core";
+import { useStaticQuery, graphql } from "gatsby";
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+} from "@material-ui/core";
 import theme from "../theme";
+import Header from "./molecules/Header/Header";
 
-// Example: https://github.com/hupe1980/gatsby-theme-material-ui/blob/master/packages/gatsby-theme-material-ui-top-layout/src/wrap-with-provider.js
-// TODO add wrap-with-provider (also see gatsby-browser.js), add helmet
+const sections = [
+  { title: "Home", url: "/" },
+  { title: "Blog", url: "/blog" },
+  { title: "About", url: "/about" },
+  { title: "Business", url: "#" },
+  { title: "Politics", url: "#" },
+  { title: "Opinion", url: "#" },
+  { title: "Science", url: "#" },
+  { title: "Health", url: "#" },
+  { title: "Style", url: "#" },
+  { title: "Travel", url: "#" },
+];
 
 const Layout = ({ pageTitle, children }) => {
   const data = useStaticQuery(graphql`
@@ -17,31 +32,22 @@ const Layout = ({ pageTitle, children }) => {
     }
   `);
 
+  const title = `${pageTitle} | ${data.site.siteMetadata.title}`;
+
   return (
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Typography variant="h3">
-          {pageTitle} | {data.site.siteMetadata.title}
-        </Typography>
+        <Container maxWidth="lg">
+          <Header title={title} sections={sections} />
 
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/blog">Blog</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-        <main>
-          <h1>{pageTitle}</h1>
           {children}
-        </main>
+        </Container>
+        {/* TODO Get footer from https://github.com/mui-org/material-ui/blob/master/docs/src/pages/getting-started/templates/blog/Blog.js */}
+        {/* <Footer
+          title="Footer"
+          description="Something here to give the footer a purpose!"
+        /> */}
       </ThemeProvider>
     </>
   );
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,19 +1,76 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import { graphql } from "gatsby";
-import { Card, Typography } from "@material-ui/core";
+import { Card, Grid, makeStyles, Typography } from "@material-ui/core";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import MainFeaturedPost from "../components/molecules/MainFeaturedPost/MainFeaturedPost";
+
+const mainFeaturedPost = {
+  title: "Title of a longer featured blog post",
+  description:
+    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+  image: "https://source.unsplash.com/random",
+  imgText: "main image description",
+  linkText: "Continue reading…",
+};
+
+const featuredPosts = [
+  {
+    title: "Featured post",
+    date: "Nov 12",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    image: "https://source.unsplash.com/random",
+    imageText: "Image Text",
+  },
+  {
+    title: "Post title",
+    date: "Nov 11",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    image: "https://source.unsplash.com/random",
+    imageText: "Image Text",
+  },
+];
+
+// Example: https://github.com/hupe1980/gatsby-theme-material-ui/blob/master/packages/gatsby-theme-material-ui-top-layout/src/wrap-with-provider.js
+// TODO add wrap-with-provider (also see gatsby-browser.js), add helmet
+const useStyles = makeStyles((theme) => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}));
 
 const BlogPage = ({ data }) => {
+  const classes = useStyles();
+
   return (
     <Layout pageTitle="My Blog Posts">
-      {data.allMdx.nodes.map((node) => (
-        <Card key={node.id}>
-          <Typography variant="h4">{node.frontmatter.title}</Typography>
-          <Typography>Posted: {node.frontmatter.date}</Typography>
-          <MDXRenderer>{node.body}</MDXRenderer>
-        </Card>
-      ))}
+      <MainFeaturedPost post={mainFeaturedPost} />
+      <Grid container spacing={4}>
+        {featuredPosts.map((post) => (
+          // <FeaturedPost key={post.title} post={post} />
+          <div>{post.title}</div>
+        ))}
+      </Grid>
+      <Grid container spacing={5} className={classes.mainGrid}>
+        {/* <Main title="From the firehose" posts={posts} /> */}
+        <main>
+          {data.allMdx.nodes.map((node) => (
+            <Card key={node.id}>
+              <Typography variant="h4">{node.frontmatter.title}</Typography>
+              <Typography>Posted: {node.frontmatter.date}</Typography>
+              <MDXRenderer>{node.body}</MDXRenderer>
+            </Card>
+          ))}
+        </main>
+        {/* <Sidebar
+          title={sidebar.title}
+          description={sidebar.description}
+          archives={sidebar.archives}
+          social={sidebar.social}
+        /> */}
+      </Grid>
     </Layout>
   );
 };
