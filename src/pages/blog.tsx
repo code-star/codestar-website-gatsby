35329a76b4feb6c2182ad172b4bad2d619a119ea
M.D. van Es <es@mdworld.nl>
2021-08-03 16:15:05 +0200

    Refactor featured posts

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -4,34 +4,7 @@ import { graphql } from "gatsby";
 import { Card, Grid, makeStyles, Typography } from "@material-ui/core";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import MainFeaturedPost from "../components/molecules/MainFeaturedPost/MainFeaturedPost";
-
-const mainFeaturedPost = {
-  title: "Title of a longer featured blog post",
-  description:
-    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
-  image: "https://source.unsplash.com/random",
-  imgText: "main image description",
-  linkText: "Continue reading…",
-};
-
-const featuredPosts = [
-  {
-    title: "Featured post",
-    date: "Nov 12",
-    description:
-      "This is a wider card with supporting text below as a natural lead-in to additional content.",
-    image: "https://source.unsplash.com/random",
-    imageText: "Image Text",
-  },
-  {
-    title: "Post title",
-    date: "Nov 11",
-    description:
-      "This is a wider card with supporting text below as a natural lead-in to additional content.",
-    image: "https://source.unsplash.com/random",
-    imageText: "Image Text",
-  },
-];
+import FeaturedPost from "../components/molecules/FeaturedPost/FeaturedPost";
 
 // Example: https://github.com/hupe1980/gatsby-theme-material-ui/blob/master/packages/gatsby-theme-material-ui-top-layout/src/wrap-with-provider.js
 // TODO add wrap-with-provider (also see gatsby-browser.js), add helmet
@@ -44,13 +17,27 @@ const useStyles = makeStyles((theme) => ({
 const BlogPage = ({ data }) => {
   const classes = useStyles();
 
+  const processedPosts = data.allMdx.nodes.map((node) => ({
+    id: node.id,
+    title: node.frontmatter.title.substr(0, 30),
+    description:
+      "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+    // image: "https://source.unsplash.com/random",
+    image: "https://images.unsplash.com/photo-1626422748187-18b829d019e2",
+    imgText: "main image description",
+    linkText: "Continue reading…",
+  }));
+
+  const mainFeaturedPost = processedPosts[0];
+  const featuredPosts = processedPosts.slice(1,3);
+
   return (
     <Layout pageTitle="My Blog Posts">
       <MainFeaturedPost post={mainFeaturedPost} />
       <Grid container spacing={4}>
         {featuredPosts.map((post) => (
-          // <FeaturedPost key={post.title} post={post} />
-          <div>{post.title}</div>
+          <FeaturedPost key={post.title} post={post} />
+          // <div key={post.id}>{post.title}</div>
         ))}
       </Grid>
       <Grid container spacing={5} className={classes.mainGrid}>
